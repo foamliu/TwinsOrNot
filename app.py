02dc27965aac06d6560e2dd0f63210abd840298f
Yang Liu <liuyang12@focusmedia.cn>
2019-02-27 11:35:00 +0800

    update

--- app.py
+++ app.py
@@ -17,15 +17,18 @@ def upload():
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        start = time.time()
-        ensure_folder('static')
-        file1 = request.files['file1']
-        file1.save('static/img0.png')
-        file2 = request.files['file2']
-        file2.save('static/img1.png')
-        theta, is_same = compare('static/img0.png', 'static/img1.png')
-        elapsed = time.time() - start
-        message = '两张照片是否同一个人: {}, 角度: {}, 时间: {} 秒。'.format(is_same, theta, elapsed)
+        try:
+            start = time.time()
+            ensure_folder('static')
+            file1 = request.files['file1']
+            file1.save('static/img0.png')
+            file2 = request.files['file2']
+            file2.save('static/img1.png')
+            theta, is_same = compare('static/img0.png', 'static/img1.png')
+            elapsed = time.time() - start
+            message = '两张照片是否同一个人: {}, 角度: {}, 时间: {} 秒。'.format(is_same, theta, elapsed)
+        except ValueError:
+            message = '对不起，没有检测到人类的脸'
         return render_template('show.html', message=message)
 
 
