36856a4a7ee9fc0d74313ae3e3a00ab3c62c9e75
Yang Liu <liuyang12@focusmedia.cn>
2019-02-27 10:10:15 +0800

    update

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,13 +1,21 @@
 # flask_web/app.py
-
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def upload():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
